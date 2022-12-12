61f23a9eb35d624926246fd33b6760dba28705ea
bwax <braxton.waxdeck@gmail.com>
2022-12-12 15:23:28 -0800

    created skeleton html and worded on classes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require("inquirer");
 const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 const employees = []
 
 function init() {
@@ -16,9 +18,9 @@ function init() {
         if (res.role == "engineer") {
             engineerQuestions()
         } else if(res.role == "intern"){
-
+            internQuestions()
         }else if(res.role == "manager"){
-            
+            managerQuestions()
         }else if(res.role == "done"){
             //make a function to generate html from responses.
         }
@@ -61,5 +63,68 @@ function engineerQuestions() {
 
     })
 
+}
+
+function managerQuestions() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name??"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your id??"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email??"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is your office number??"
+        }
+    ]).then((res) => {
+        const manager = new Manager(res.name, res.id, res.email, res.officeNumber);
+        console.log(manager);
+        employees.push(manager);
+        console.log(manager);
+        init();
+
+    })
+}
+function internQuestions() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name??"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is your id??"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email??"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the name of your school??"
+        }
+    ]).then((res) => {
+        const intern = new Intern(res.name, res.id, res.email, res.school);
+        console.log(intern);
+        employees.push(intern);
+        console.log(intern);
+        init();
+
+    })
 }
 init();
